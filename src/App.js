4a7040569cc4e2dac3011abc998f97690c8cffb1
Steven Woodward <steveanthony999@gmail.com>
2021-02-02 00:58:27 -0800

    added signup route and created signup components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Signup from './components/auth/Signup';
 
 function App() {
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        {/* Auth */}
+        <Route path='/signup' component={Signup} />
+      </Switch>
     </Router>
   );
 }
